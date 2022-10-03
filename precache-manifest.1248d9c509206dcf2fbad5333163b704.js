e161736a732f62232ddb1afa9936ba888a5246f2
Daniel Borlay <Daniel_Borlay@ephubudw05dd.home>
2022-10-03 12:32:24 +0200

    Updates

---
+++ precache-manifest.1248d9c509206dcf2fbad5333163b704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "687b1c6d4749e97be416",
+    "url": "/static/css/main.b1c21566.chunk.css"
+  },
+  {
+    "revision": "687b1c6d4749e97be416",
+    "url": "/static/js/main.f38265d0.chunk.js"
+  },
+  {
+    "revision": "62b547234d3f153c3447",
+    "url": "/static/js/runtime~main.be2f2222.js"
+  },
+  {
+    "revision": "81a4f1ab6a56c21e0aaf",
+    "url": "/static/js/2.f506bf8f.chunk.js"
+  },
+  {
+    "revision": "f610709ef7b15d7cd37f",
+    "url": "/static/js/3.c3bc911a.chunk.js"
+  },
+  {
+    "revision": "b8d169baacc991da9966941fa0f5afed",
+    "url": "/index.html"
+  }
+];
